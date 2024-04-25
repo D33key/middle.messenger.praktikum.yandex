7798089c89c98d0c01f6cf1a97c41103fa840c60
unknown <dimakisa777@yandex.ru>
2024-04-25 10:04:23 +0300

    feat: trying to make new templator by using logic react

---
+++ src/Templater/newVersion.ts
@@ -0,0 +1,9 @@
+class Slowact {
+	constructor(root: HTMLElement, component: any) {}
+
+	public createElement(
+		type: keyof HTMLElementTagNameMap,
+		attributes,
+		children,
+	) {}
+}
--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,6 @@
 import { Templator } from './Templater';
 import './style.css';
 
-const TestDiv = () => {
-	return `
-  <div>
-    <h1>Example</h1>
-    ${TestBtn1()}
-    ${TestBtn2()}
-  </div>`;
-};
+// const templator = new Templator(TestDiv, {});
 
-const TestBtn1 = () => {
-	const handleClick = (e: Event) => {
-		console.log('CLICK WORKS in TestBtn1!');
-	};
-	return `
-  <div>
-    <p>Wow test1!</p>
-    <button data-event="click" data-event-function='${handleClick}'>Click</button>
-  </div>`;
-};
-
-const TestBtn2 = () => {
-	const handleClick = (e: Event) => {
-		console.log('CLICK WORKS in TestBtn2!');
-	};
-	return `
-  <div>
-    <p>Wow test2!</p>
-    <button data-event="click" data-event-function='${handleClick}'>Click</button>
-  </div>`;
-};
-
-const templator = new Templator(TestDiv, {});
-
-templator.compile();
+// templator.compile();
