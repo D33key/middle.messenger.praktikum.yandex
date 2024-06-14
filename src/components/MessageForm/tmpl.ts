798fbeb3d0be497c86b2240be6d78e238c8854c0
unknown <dimakisa777@yandex.ru>
2024-06-15 01:53:15 +0300

    fix: remove file upload (not ready)

--- src/components/MessageForm/tmpl.ts
+++ src/components/MessageForm/tmpl.ts
@@ -1,7 +1,8 @@
 export const template = /* html */ `
   <form class='message-input'>
-    <img src="/assets/sendFile.png" alt='choose file'/>
     <input name='message' type='text' placeholder='Введите сообщение...'/>
     <button type='submit'>Отправка</button>
   </form>
 `;
+
+// <img src="/assets/sendFile.png" alt='choose file'/>
